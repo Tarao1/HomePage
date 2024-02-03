49461664b7021db5b69c5c71cb62555458dfbb5f
Tarao1 <take.ngz1@gmail.com>
2024-02-03 21:49:58 +0900

    add images

--- src/componets/about_detail.tsx
+++ src/componets/about_detail.tsx
@@ -1,6 +1,9 @@
 import styles from "../styles/about_detail.module.css";
-import topImage from "../images/speak1.jpeg";
-import topImage2 from "../images/speak2.jpeg";
+import imagePath1 from "../images/about1.jpg";
+import imagePath2 from "../images/about2.jpg";
+import imagePath3 from "../images/about3.jpg";
+import imagePath4 from "../images/about4.jpg";
+import imagePath5 from "../images/about5.jpg";
 import Image from "next/image";
 
 export default function AboutDetail({
@@ -8,12 +11,29 @@ export default function AboutDetail({
   title,
   descript,
   direction,
+  imageNo,
 }: {
   no: string;
   title: string;
   descript: string;
   direction: number;
+  imageNo: number;
 }) {
+  var imagePath = imagePath1;
+  switch (imageNo) {
+    case 2:
+      imagePath = imagePath2;
+      break;
+    case 3:
+      imagePath = imagePath3;
+      break;
+    case 4:
+      imagePath = imagePath4;
+      break;
+    case 5:
+      imagePath = imagePath5;
+      break;
+  }
   if (direction === 1) {
     // 左揃え
     return (
@@ -23,7 +43,13 @@ export default function AboutDetail({
           <p className={styles.number_box_no}>{no}</p>
         </div>
         <div className={styles.image_left}>
-          <Image src={topImage} alt="" layout="responsive" />
+          <Image
+            src={imagePath}
+            alt=""
+            layout="responsive"
+            width={100}
+            height={100}
+          />
         </div>
         <h2 className={styles.title_left}>{title}</h2>
         <p className={styles.p_left}>{descript}</p>
@@ -38,7 +64,13 @@ export default function AboutDetail({
           <p className={styles.number_box_no}>{no}</p>
         </div>
         <div className={styles.image_right}>
-          <Image src={topImage2} alt="" layout="responsive" />
+          <Image
+            src={imagePath}
+            alt=""
+            layout="responsive"
+            width={100}
+            height={100}
+          />
         </div>
         <h2 className={styles.title_right}>{title}</h2>
         <p className={styles.p_right}>{descript}</p>
