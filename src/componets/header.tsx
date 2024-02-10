07323155046ecb505b531559b66f9114335c44db
Tarao1 <take.ngz1@gmail.com>
2024-02-10 23:15:53 +0900

    edit pc layout

--- src/componets/header.tsx
+++ src/componets/header.tsx
@@ -1,7 +1,6 @@
 import Container from "./container";
 import styles from "../styles/header.module.css";
 import React from "react";
-import Button from "./button";
 
 export default function Header() {
   return (
